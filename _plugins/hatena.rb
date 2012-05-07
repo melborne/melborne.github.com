aaba68a2706be1853995dea326a86b573615d4d6
kyoendo <postagie@gmail.com>
2012-05-07 22:00:00 +0900

    add description

--- _plugins/hatena.rb
+++ _plugins/hatena.rb
@@ -1,3 +1,8 @@
+# A Liquid tag for Jekyll sites that allows embedding Hatena Bookmark counter.
+# by: Kyo Endo
+# Source URL: https://gist.github.com/2565302
+#
+# Example usage: {% hatebu http://yourblog.com/path_to_a_post/ Title of Post %}
 module Jekyll
   class HatenaBookmarkTag < Liquid::Tag
     def initialize(tag_name, text, token)
@@ -7,6 +12,7 @@ module Jekyll
 
     def render(context)
       url, title = @text.match(/(https?:\/\/\S+)(.*)/){ [$1, $2] }
+      return "" unless url
       bm_url = %{<a href="http://b.hatena.ne.jp/entry/#{url}" class="http-bookmark" target="_blank"><img src="http://b.hatena.ne.jp/entry/image/#{url}" alt="" class="http-bookmark"></a>}
 
       unless title.nil?
@@ -19,4 +25,3 @@ module Jekyll
 end
 
 Liquid::Template.register_tag('hatebu', Jekyll::HatenaBookmarkTag)
-
